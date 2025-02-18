9de3d4a61dedca4eb6c6855751f6e1e4f975de7b
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-02-18 16:46:57 +0100

    forgot to add commentrepository

--- App/Repository/CommentRepository.php
+++ App/Repository/CommentRepository.php
@@ -9,14 +9,14 @@ use App\Tools\StringTools;
 class CommentRepository extends Repository
 {
   public function findAllByArticleId(int $article_id)
-    {
-        $query = $this->pdo->prepare("SELECT * FROM comment");
-        $query->execute();
-        $comments = $query->fetchAll($this->pdo::FETCH_ASSOC);
-        $commentsObjects = [];
-        foreach($comments as $comment) {
-            $commentsObjects[] = Comment::createAndHydrate($comment);;
-        }
-        return $commentsObjects;
+  {
+    $query = $this->pdo->prepare("SELECT * FROM comment");
+    $query->execute();
+    $comments = $query->fetchAll($this->pdo::FETCH_ASSOC);
+    $commentsObjects = [];
+    foreach($comments as $comment) {
+      $commentsObjects[] = Comment::createAndHydrate($comment);;
     }
+    return $commentsObjects;
+  }
 }
